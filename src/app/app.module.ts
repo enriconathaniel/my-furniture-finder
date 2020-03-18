73922c0f654dd26d9f5f31fe4d81848eb7cfaf6a
Enrico <enrico@esb.co.id>
2020-03-18 20:25:57 +0700

    Furniture Finder
    pretest Fabelio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material.module';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MaterialModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormControl } from "@angular/forms";
+import * as _ from "lodash";
+interface DeliveryOption {
+  label: string;
+  value: number;
+}
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+  filterTimer: any;
+  filterName: string;
+  filterStyle: any;
+  filterDelivery: any;
+  dataSource: any;
+  placeholderName: string;
+  furnitureStyle: any;
+  deliveryOption: Array<DeliveryOption> = [
+    { label: "1 week", value: 7 },
+    { label: "2 weeks", value: 14 },
+    { label: "1 month", value: 30 }
+  ];
+  products: any;
+  dataFurniture: any;
+  furnitureStyleControl = new FormControl();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.placeholderName = "Search Furniture";
+    this.products = this.http
+      .get("http://www.mocky.io/v2/5c9105cb330000112b649af8")
+      .subscribe((data: any) => {
+        this.products = data;
+        this.dataFurniture = _.cloneDeep(this.products);
+        this.furnitureStyle = this.http
+          .get("http://www.mocky.io/v2/5c9105cb330000112b649af8")
+          .subscribe((dataStyle: any) => {
+            this.furnitureStyle = dataStyle.furniture_styles;
+          });
+      });
+  }
+
+  onChangeFilter() {
+    let styleFilters;
+    let deliveryFilters;
+
+    if (this.filterStyle) {
+      styleFilters = this.filterStyle.map(option => option).join(", ");
+    }
+    if (this.filterDelivery) {
+      deliveryFilters = this.filterDelivery
+        .map(option => option.value)
+        .join(", ");
+    }
+
+    let haha = this.products.products.filter(product => {
+      let deliveryTime = this.filterDelivery
+        ? this.filterDelivery.map(option => option.value)
+        : [];
+      let maxDeliveryTime =
+        deliveryTime.length > 0 ? Math.max(deliveryTime) : null;
+      if (!maxDeliveryTime && (!this.filterStyle || this.filterStyle.length === 0) ) {
+        return product;
+      }
+      if (!maxDeliveryTime) {
+        return product.furniture_style.some(item =>
+          this.filterStyle.includes(item)
+        );
+      } else if (!this.filterStyle || this.filterStyle.length === 0) {
+        return product.delivery_time <= maxDeliveryTime;
+      } else {
+        return (
+          product.furniture_style.some(item =>
+            this.filterStyle.includes(item)
+          ) && product.delivery_time <= maxDeliveryTime
+        );
+      }
+    });
+    this.dataFurniture.products = haha;
+  }
+}
